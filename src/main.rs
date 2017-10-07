394d9855107a97946565043362e0cd8fec0a0e38
matthew86707 <matt86707@Gmail.com>
2017-10-07 11:07:50 -0700

    Added some shader code

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
     let display = glium::Display::new(window, context, &events_loop).unwrap();
 
 	let vertex_shader_src = r#"
-    #version 140
+    #version 330
 
     in vec2 position;
     in vec3 color;
@@ -32,14 +32,26 @@ fn main() {
 "#;
 
 let fragment_shader_src = r#"
-    #version 140
+    #version 330
 
     uniform float t;
+    uniform float b;
     in vec3 colorPassed;
     out vec4 fragColor;
 
     void main() {
-      	fragColor = vec4(gl_FragCoord.x / 800, gl_FragCoord.y / 600, t, 1.0);
+        float ts = t / 2000;
+
+        float divisionsX = 200.0f - t / 200;
+        float divisionsY = 200.0f - t / 200;
+
+        float modXFracPiA = (mod(gl_FragCoord.x, divisionsX) / divisionsX) * 3.14;
+        float modYFracPiA = (mod(gl_FragCoord.y, divisionsY) / divisionsY) * 3.14;
+
+        float modXFracPiB = (mod(gl_FragCoord.x + divisionsX / 2, divisionsX) / divisionsX) * 3.14;
+        float modYFracPiB = (mod(gl_FragCoord.y + divisionsY / 2, divisionsY) / divisionsY) * 3.14;
+
+      	fragColor = vec4((sin(ts) / 1.57) * (sin(modXFracPiA) / 1.57) + ((-sin(ts) / 1.57) * (sin(modYFracPiB) / 1.57)), ((sin(ts) / 1.57) * (sin(modXFracPiB) / 1.57)) + ((-sin(ts) / 1.57) * (sin(modYFracPiA) / 1.57)), b, 1.0);
     }
 "#;
 
@@ -60,15 +72,15 @@ let program = glium::Program::from_source(&display, vertex_shader_src, fragment_
 	let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
 	let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
-
+    let mut blueVal : f32 = 0.5;
 
     let mut closed = false;
     while !closed {
-    	t += 0.0016;
+    	t += 0.16;
         let mut target = display.draw();
         let mut red_value : f32 = 0.0;
         target.clear_color(red_value, 0.2, 0.0, 1.0);
-         target.draw(&vertex_buffer, &indices, &program, &uniform! { t: t },
+         target.draw(&vertex_buffer, &indices, &program, &uniform! { t: t , b : blueVal},
             &Default::default()).unwrap();
         target.finish().unwrap();
 
@@ -80,6 +92,8 @@ let program = glium::Program::from_source(&display, vertex_shader_src, fragment_
                     glutin::WindowEvent::KeyboardInput { input, .. } =>  match input.virtual_keycode { 
                     	Some(VirtualKeyCode::Escape) => {closed = true;}, 
                     	Some(VirtualKeyCode::A) => {t = 0.0;},
+                        Some(VirtualKeyCode::Q) => {blueVal = blueVal + 0.1},
+                        Some(VirtualKeyCode::E) => {blueVal = blueVal - 0.1},
                     	_ => () },
                   	_ => (),
                 },
